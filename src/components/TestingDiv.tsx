93bf468582f976cf3dfdb2cf3407c903f6279438
dev-kagant <gant.kerri@gmail.com>
2026-01-30 05:45:27 -0500

    start figuring out animations

---
+++ src/components/TestingDiv.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+function TestingDiv() {
+    const [bounce, setBounce] = useState('start-bounce');
+    const [ballSize, setBallSize] = useState('');
+    const [fontSize, setFontSize] = useState('');
+
+    const handleBounce = () => {
+        if(bounce === 'start-bounce'){
+            setBounce('end-bounce');
+        }
+        if(bounce === 'end-bounce'){
+            setBounce('start-bounce');
+        }
+    }
+
+    const handleSize = () => {
+        if(fontSize === ''||'end-grow'){
+            setFontSize('start-grow');
+            setBallSize('start-ball-shrink')
+        }
+        if(fontSize === 'start-grow'){
+            setFontSize('end-grow');
+            setBallSize('end-ball-shrink')
+        }
+    }
+
+  return (
+    <div className='page-box'>
+        <div className={`ball ${bounce} ${ballSize}`}></div>
+        <div className={`joke ${fontSize}`}>There is a joke here</div>
+        <div className='buttons'>
+            <button className='button-style' onClick={handleSize}>Answer</button>
+            <button className='button-style' onClick={handleBounce}>Next</button>
+        </div>
+    </div>
+  )
+}
+
+export default TestingDiv
