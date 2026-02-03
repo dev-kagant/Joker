056adca28ce498b9ba5e5cf5ac698a5ecca1c403
dev-kagant <gant.kerri@gmail.com>
2026-02-03 06:05:16 -0500

    style buttons

--- src/components/JokeComponent.tsx
+++ src/components/JokeComponent.tsx
@@ -30,9 +30,9 @@ function JokeComponent() {
         <div className={`z-2 rounded-full bg-joker-200 text-joker-100 transform-[translate(-50%, -50%)] absolute ${ballOut}`}></div>
         <div className={`z-3 absolute text-center text-joker-100 top-[30%] left-[50%] font-quantico font-bold transform-[translate(-50%, -50%)] ${joke}`}>Jokes to be shared, and more jokes to come, just hit next.</div>
         <div className={`z-3 text-[0px] absolute text-center text-joker-100 top-[30%] left-[50%] font-quantico font-bold transform-[translate(-50%, -50%)] ${punchline}`}>Answers to the jokes to be shared, and more punch lines to come, just hit next.</div>
-        <div className='buttons'>
-            <button className='button-style' onClick={handlePunchline}>Punchline</button>
-            <button className='button-style' onClick={handleNextJoke}>Next Joke</button>
+        <div className='flex flex-row justify-evenly items-center absolute top-[60%] w-full'>
+            <button className='h-[50px] w-auto p-2 bg-joker-100 rounded-lg border-1 border-joker-200' onClick={handlePunchline}>Punchline</button>
+            <button className='h-[50px] w-auto p-2 bg-joker-100 rounded-lg border-1 border-joker-200' onClick={handleNextJoke}>Next Joke</button>
         </div>
     </div>
   )
