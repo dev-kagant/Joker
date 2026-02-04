8e3e82275b7ffb6618d256257628c2a6074516e7
dev-kagant <gant.kerri@gmail.com>
2026-02-04 06:04:27 -0500

    complete joke component for deployment

--- src/components/JokeComponent.tsx
+++ src/components/JokeComponent.tsx
@@ -1,38 +1,76 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
 
 function JokeComponent() {
+    const [mount, setMount] = useState(true);
     const [ballIn, setBallIn] = useState('ball-drop-in');
     const [ballOut, setBallOut] = useState('');
-    const [joke, setJoke] = useState('joke-grow');
+    const [setup, setSetup] = useState('');
+    const [delivery, setDelivery] = useState('')
+    const [joke, setJoke] = useState('');
     const [punchline, setPunchline] = useState('');
+    const [jokeStyles, setJokeStyles] = useState('joke-grow');
+    const [punchlineStyles, setPunchlineStyles] = useState('');
 
-    const fetchJoke = () => {
+    useEffect(()=>{
+        if(mount){
+            setMount(false);
+            fetchFirstJoke();
+            fetchJoke();
+        }
+    },[])
 
+    const fetchFirstJoke = async() => {
+        await axios.get('https://v2.jokeapi.dev/joke/Any?type=twopart&safe-mode')
+            .then((response)=>{
+                setJoke(response.data.setup);
+                setPunchline(response.data.delivery);
+            })
+            .catch((error)=>{
+                console.log("Error: ", error);
+            })
+    }
+
+    const fetchJoke = async() => {
+        await axios.get('https://v2.jokeapi.dev/joke/Any?type=twopart&safe-mode')
+            .then((response)=>{
+                setSetup(response.data.setup);
+                setDelivery(response.data.delivery);
+            })
+            .catch((error)=>{
+                console.log("Error: ", error);
+            })
     }
 
     const handleNextJoke = () => {
         setBallOut("ball-drop-out");
         setBallIn("ball-drop-in");
-        setJoke("joke-grow");
-        setPunchline("joke-shrink");
+        setJokeStyles("joke-grow");
+        setPunchlineStyles("joke-shrink");
+        setTimeout(()=>{
+            setJoke(setup);
+            setPunchline(delivery);
+            fetchJoke();
+        }, 2000)
     }
 
     const handlePunchline = () => {
         setBallIn("ball-bounce");
         setBallOut("");
-        setJoke("joke-shrink");
-        setPunchline("joke-grow");
+        setJokeStyles("joke-shrink");
+        setPunchlineStyles("joke-bounce");
     }
 
   return (
     <div className='h-full w-full relative'>
         <div className={`z-2 h-4 w-full rounded-full bg-joker-200 text-joker-100 transform-[translate(-50%, -50%)] absolute ${ballIn}`}></div>
         <div className={`z-2 rounded-full bg-joker-200 text-joker-100 transform-[translate(-50%, -50%)] absolute ${ballOut}`}></div>
-        <div className={`z-3 absolute text-center text-joker-100 top-[30%] left-[50%] font-quantico font-bold transform-[translate(-50%, -50%)] ${joke}`}>Jokes to be shared, and more jokes to come, just hit next.</div>
-        <div className={`z-3 text-[0px] absolute text-center text-joker-100 top-[30%] left-[50%] font-quantico font-bold transform-[translate(-50%, -50%)] ${punchline}`}>Answers to the jokes to be shared, and more punch lines to come, just hit next.</div>
+        <div className={`z-3 absolute text-center text-joker-100 top-[30%] left-[50%] font-quantico font-bold transform-[translate(-50%, -50%)] ${jokeStyles}`}>{joke}</div>
+        <div className={`z-3 text-[0px] absolute text-center text-joker-100 top-[30%] left-[50%] font-quantico font-bold transform-[translate(-50%, -50%)] ${punchlineStyles}`}>{punchline}</div>
         <div className='flex flex-row justify-evenly items-center absolute top-[60%] w-full'>
-            <button className='h-[50px] w-auto p-2 bg-joker-100 rounded-lg border-1 border-joker-200' onClick={handlePunchline}>Punchline</button>
-            <button className='h-[50px] w-auto p-2 bg-joker-100 rounded-lg border-1 border-joker-200' onClick={handleNextJoke}>Next Joke</button>
+            <button className='h-[50px] sm:h-[60px] lg:h-[70px] w-[110px] sm:w-[140px] lg:w-[170px] font-quantico text-lg sm:text-2xl lg:text-3xl p-2 bg-joker-100 rounded-lg border-1 lg:border-2 border-joker-200 cursor-pointer' onClick={handlePunchline}>Punchline</button>
+            <button className='h-[50px] sm:h-[60px] lg:h-[70px] w-[110px] sm:w-[140px] lg:w-[170px] font-quantico text-lg sm:text-2xl lg:text-3xl p-2 bg-joker-100 rounded-lg border-1 lg:border-2 border-joker-200 cursor-pointer' onClick={handleNextJoke}>Next Joke</button>
         </div>
     </div>
   )
